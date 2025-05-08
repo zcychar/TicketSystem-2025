cda5d1c3d68a78bb995b96ec6fdf524219c0e4d1
zcychar <zhang.77274@gmail.com>
2025-05-08 15:54:00 +0800

    Update: Test usage

--- src/include/common/config.h
+++ src/include/common/config.h
@@ -9,7 +9,7 @@ namespace sjtu {
 static constexpr int INVALID_FRAME_ID = -1;  // invalid frame id
 static constexpr int INVALID_PAGE_ID = -1;   // invalid page id
 
-static constexpr int SJTU_PAGE_SIZE = 48;                                        // size of a data page in byte
+static constexpr int SJTU_PAGE_SIZE = 4096*3;                                        // size of a data page in byte
 static constexpr int BUFFER_POOL_SIZE = 128;                                         // size of buffer pool
 static constexpr int DEFAULT_DB_IO_SIZE = 16;                                        // starting size of file on disk
 static constexpr int LRUK_REPLACER_K = 10;                                           // backward k-distance for lru-k

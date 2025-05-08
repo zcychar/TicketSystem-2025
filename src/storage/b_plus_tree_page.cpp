106dae6031df8e1b5ee0c3db9ebe239098dbfebd
zcychar <zhang.77274@gmail.com>
2025-05-08 15:23:03 +0800

    Update: Test usage

--- src/storage/b_plus_tree_page.cpp
+++ src/storage/b_plus_tree_page.cpp
@@ -1,33 +1,31 @@
 #include "storage/b_plus_tree_page.h"
 
 namespace sjtu {
+  /*
+   * Helper methods to get/set page type
+   * Page type enum class is defined in b_plus_tree_page.h
+   */
+  auto BPlusTreePage::IsLeafPage() const -> bool { return page_type_ == IndexPageType::LEAF_PAGE; }
+  void BPlusTreePage::SetPageType(IndexPageType page_type) { page_type_ = page_type; }
 
-/*
- * Helper methods to get/set page type
- * Page type enum class is defined in b_plus_tree_page.h
- */
-auto BPlusTreePage::IsLeafPage() const -> bool { return page_type_ == IndexPageType::LEAF_PAGE; }
-void BPlusTreePage::SetPageType(IndexPageType page_type) { page_type_ = page_type; }
+  /*
+   * Helper methods to get/set size (number of key/value pairs stored in that
+   * page)
+   */
+  auto BPlusTreePage::GetSize() const -> int { return size_; }
+  void BPlusTreePage::SetSize(int size) { size_ = size; }
+  void BPlusTreePage::ChangeSizeBy(int amount) { size_ += amount; }
 
-/*
- * Helper methods to get/set size (number of key/value pairs stored in that
- * page)
- */
-auto BPlusTreePage::GetSize() const -> int { return size_; }
-void BPlusTreePage::SetSize(int size) { size_ = size; }
-void BPlusTreePage::ChangeSizeBy(int amount) { size_ += amount; }
+  /*
+   * Helper methods to get/set max size (capacity) of the page
+   */
+  auto BPlusTreePage::GetMaxSize() const -> int { return max_size_; }
+  void BPlusTreePage::SetMaxSize(int size) { max_size_ = size; }
 
-/*
- * Helper methods to get/set max size (capacity) of the page
- */
-auto BPlusTreePage::GetMaxSize() const -> int { return max_size_; }
-void BPlusTreePage::SetMaxSize(int size) { max_size_ = size; }
-
-/*
- * Helper method to get min page size
- * Generally, min page size == max page size / 2
- * But whether you will take ceil() or floor() depends on your implementation
- */
-auto BPlusTreePage::GetMinSize() const -> int { return (max_size_ ) / 2; }
-
-}  // namespace sjtu
+  /*
+   * Helper method to get min page size
+   * Generally, min page size == max page size / 2
+   * But whether you will take ceil() or floor() depends on your implementation
+   */
+  auto BPlusTreePage::GetMinSize() const -> int { return (max_size_ + 1) / 2; }
+} // namespace sjtu

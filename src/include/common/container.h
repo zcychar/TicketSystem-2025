5018449b92556daaa76cdad070d2d0c7a3d00314
zcychar <zhang.77274@gmail.com>
2025-05-09 12:48:00 +0800

    Reapply "perf: enhance hash function"
    This reverts commit ca101ca05dd99401828ee613dc62d8d7b02f0ea9.

--- src/include/common/container.h
+++ src/include/common/container.h
@@ -4,53 +4,48 @@
 #include <string>
 
 namespace sjtu {
-  constexpr int MOD = 1e9 + 7;
-  constexpr int BASE = 19260817;
+  constexpr int MOD_1 = 1e9 + 7;
+  constexpr int BASE_1 = 19260817;
+
+  constexpr int MOD_2 = 1e9 + 9;
+  constexpr int BASE_2 = 12255871;
 
   struct Key {
-    int hashed_key = 0;
+    int hashed_key_1 = 0;
+    int hashed_key_2 = 0;
     char key_[65];
     int value;
 
+
     Key(const std::string &key, const int value = -2147483648): value(value) {
+      hashed_key_1 = 0;
+      hashed_key_2 = 0;
       auto size = key.size();
       for (int i = 0; i < size; ++i) {
-        key_[i]=key[i];
-      }
-      for(int i=size;i<=64;++i) {
-        key_[i]='\0';
+        hashed_key_1 = (1ll * hashed_key_1 * BASE_1 + key[i]) % MOD_1;
+        hashed_key_2 = (1ll * hashed_key_2 * BASE_2 + key[i]) % MOD_2;
       }
     }
 
-    // Key(const std::string &key, const int value = -2147483648): value(value) {
-    //   hashed_key = 0;
-    //   auto size = key.size();
-    //   for (int i = 0; i < size; ++i) {
-    //     hashed_key = (1ll * hashed_key * BASE + key[i]) % MOD;
-    //   }
-    // }
-    //
     friend std::ostream &operator<<(std::ostream &os, const Key &x) {
-      os << x.hashed_key;
+      os << x.hashed_key_1;
       return os;
     }
   };
 
   struct Comparator {
     int operator ()(const Key &lhs, const Key &rhs) {
-      // if (lhs.hashed_key > rhs.hashed_key) {
-      //   return 1;
-      // }
-      // if (lhs.hashed_key < rhs.hashed_key) {
-      //   return -1;
-      // }
-      for(int i=0;i<64;++i) {
-        if(lhs.key_[i]!=rhs.key_[i]) {
-          if(lhs.key_[i]<rhs.key_[i]) {
-            return -1;
-          }
-          return 1;
-        }
+      if (lhs.hashed_key_1 > rhs.hashed_key_1) {
+        return 1;
+      }
+      if (lhs.hashed_key_1 < rhs.hashed_key_1) {
+        return -1;
+      }
+      if (lhs.hashed_key_2 > rhs.hashed_key_2) {
+        return 1;
+      }
+      if (lhs.hashed_key_2 < rhs.hashed_key_2) {
+        return -1;
       }
       if (lhs.value < rhs.value) {
         return -1;
@@ -64,20 +59,19 @@ namespace sjtu {
 
   struct DegradedComparator {
     int operator ()(const Key &lhs, const Key &rhs) {
-      // if (lhs.hashed_key > rhs.hashed_key) {
-      //   return 1;
-      // }
-      // if (lhs.hashed_key < rhs.hashed_key) {
-      //   return -1;
-      // }
-      for(int i=0;i<64;++i) {
-        if(lhs.key_[i]!=rhs.key_[i]) {
-          if(lhs.key_[i]<rhs.key_[i]) {
-            return -1;
-          }
-          return 1;
-        }
+      if (lhs.hashed_key_1 > rhs.hashed_key_1) {
+        return 1;
       }
+      if (lhs.hashed_key_1 < rhs.hashed_key_1) {
+        return -1;
+      }
+      if (lhs.hashed_key_2 > rhs.hashed_key_2) {
+        return 1;
+      }
+      if (lhs.hashed_key_2 < rhs.hashed_key_2) {
+        return -1;
+      }
+
       return 0;
     }
   };
